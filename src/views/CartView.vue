ebbfab0c16b3cb4386391ac5e650e596f9ba37aa
sanzdecastro <sanzdecastro@gmail.com>
2024-08-08 16:47:17 +0200

    cart blocks

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -73,12 +73,14 @@ export default {
 
 <template>
   <div class="cart">
-    <h1>This is an cart page</h1>
+    <h1>Encarga tus láminas contactando con el/la autorx de las fotografías seleccionadas</h1>
 
     <div class="autor-block duna" v-if="itemsDuna.length > 0">
+      <h3>Seleccionadas de Duna Vallés</h3>
       <div class="item" v-if="itemsDuna.length > 0" v-for="item in itemsDuna">
         <div class="image-container">
-            <img :src="this.photoUrl  + item.photo + '&sz=w600-h600'"  alt="">
+          <h2>Seleccionadas de Duna Vallés</h2>
+          <img :src="this.photoUrl  + item.photo + '&sz=w600-h600'"  alt="">
           </div>
           <div class="item-info">
             <h2>{{ item.title }}</h2>
@@ -91,12 +93,13 @@ export default {
         </div>
        
       </div>
-      <p>Total Price: {{ this.totalDuna }}</p>
+      <p>Precio total: {{ this.totalDuna }}</p>
         
         <!-- <PayPalDonateButton/> -->
     </div>
 
     <div class="autor-block lorenzo" v-if="itemsLorenzo.length > 0">
+      <h3>Seleccionadas de Lorenzo de Nicola</h3>
       <div class="item" v-if="itemsLorenzo.length > 0" v-for="item in itemsLorenzo">
         <div class="image-container">
             <img :src="this.photoUrl  + item.photo + '&sz=w600-h600'"  alt="">
@@ -112,11 +115,12 @@ export default {
         </div>
        
       </div>
-      <p>Total Price: {{ this.totalLorenzo }}</p>
+      <p>Precio total: {{ this.totalLorenzo }}</p>
         <!-- <PayPalDonateButton/> -->
     </div>
 
     <div class="autor-block paula" v-if="itemsPaula.length > 0">
+      <h3>Seleccionadas de Paula Grácia</h3>
       <div class="item" v-if="itemsPaula.length > 0" v-for="item in itemsPaula">
         <div class="image-container">
             <img :src="this.photoUrl  + item.photo + '&sz=w600-h600'"  alt="">
@@ -132,14 +136,16 @@ export default {
         </div>
        
       </div>
-      <p>Total Price: {{ this.totalPaula }}</p>
+      <p>Precio total: {{ this.totalPaula }}</p>
         <!-- <PayPalDonateButton/> -->
     </div>
 
     <div class="autor-block santi" v-if="itemsSanti.length > 0">
+      <h3>Seleccionadas de Santi Sánchez</h3>
       <div class="item" v-if="itemsSanti.length > 0" v-for="item in itemsSanti">
+        
         <div class="image-container">
-            <img :src="this.photoUrl  + item.photo + '&sz=w600-h600'"  alt="">
+          <img :src="this.photoUrl  + item.photo + '&sz=w600-h600'"  alt="">
           </div>
             <div class="item-info">
             <h2>{{ item.title }}</h2>
@@ -152,7 +158,7 @@ export default {
         </div>
         
       </div>
-      <p>Total Price: {{ this.totalSanti }}</p>
+      <p>Precio total: {{ this.totalSanti }}</p>
         <!-- <PayPalDonateButtonSanti/> -->
         <a class="button" :href="'https://wa.me/+34695501618?text=Estoy%20interesada%20en%20' + this.itemsSanti" target="_blank" style="display:inline-block;background-color:#25d366;padding:10px 24px;color:#fff;font-size:16px;line-height:1.5;border-radius:2px;text-decoration:none;">Contactar con Santi Sánchez</a>
     </div>
@@ -163,11 +169,92 @@ export default {
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+
+  
+.cart {
+  margin-top: max(200 * var(--r), 200px);
+  padding: max(20 * var(--r), 20px);
+  padding-bottom: max(100 * var(--r), 100px);
+  @apply
+  relative
+  bg-white;
+  &:after {
+    content: '';
+    @apply
+    absolute
+    w-full
+    h-6
+    left-0
+    -top-6
+    bg-gradient-to-b
+    from-transparent
+    to-white;
+  }
+  @media screen and (min-width: 1200px) {
+    max-width: 500px;
+    margin: 0 auto;
+    margin-top: max(200 * var(--r), 200px);
+    
   }
 }
+
+.cart h1 {
+  font-size: max(20 * var(--r), 20px);
+  line-height: max(24 * var(--r), 24px);
+  margin-bottom: max(40 * var(--r), 40px);
+}
+
+.cart .autor-block {
+  @apply
+  flex
+  gap-2
+  flex-col;
+}
+
+
+
+.cart .autor-block .item {
+  @apply
+  flex
+  gap-2;
+}
+
+.cart .autor-block h2 {
+  font-size: max(30 * var(--r), 30px);
+  line-height: max(30 * var(--r), 30px);
+}
+
+.cart .autor-block h3 {
+  font-family: 'Sometimes';
+  font-size: max(24 * var(--r), 24px);
+  line-height: max(28 * var(--r), 28px);
+  margin-bottom: max(10 * var(--r), 10px);
+}
+
+.cart .autor-block > h3 {
+  font-family: 'Atlas';
+  font-size: max(20 * var(--r), 20px);
+  line-height: max(24 * var(--r), 24px);
+  border-top: 1px solid black;
+  padding-top: max(10 * var(--r), 10px);
+  padding-bottom: max(20 * var(--r), 20px);
+}
+
+.cart .autor-block .options {
+  @apply
+  flex
+  gap-0
+  flex-col;
+}
+
+.cart .autor-block .options span {
+  @apply
+  block;
+}
+
+.cart .autor-block .item .image-container, .cart .autor-block .item .item-info {
+  @apply
+  flex-1;
+}
+
 </style>
